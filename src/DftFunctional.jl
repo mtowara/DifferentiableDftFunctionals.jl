5f5301a24eba416420409545f8c32681500a36ec
Markus Towara <mtowara@gmail.com>
2022-09-29 17:38:12 +0200

    Update DftFunctional.jl

--- src/DftFunctional.jl
+++ src/DftFunctional.jl
@@ -4,6 +4,6 @@ ones supported by [Libxc](https://tddft.org/programs/libxc) as symbols,
 e.g. `:lda_x`, `hyb_gga_xc_b3lyp`. Only a subset of functionals is currently
 available.
 """
-function DftFunctional(identifier::Symbol, args...; kwargs...)
+function DifferentiableDftFunctional(identifier::Symbol, args...; kwargs...)
     DftFunctional(Val(identifier), args...; kwargs...)
 end
